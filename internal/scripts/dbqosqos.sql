3f4efd10cf8071284fc029a9746b6d6fc6eab52c
darwinTtito <54154740+darwinTtito@users.noreply.github.com>
2019-10-12 19:18:16 -0300

    fix: improve the database script (#19)

--- internal/scripts/dbqosqos.sql
+++ internal/scripts/dbqosqos.sql
@@ -24,7 +24,7 @@ ENGINE = InnoDB;
 -- Table `dbqosqos`.`album`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `dbqosqos`.`album` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(200) NULL,
   `description` VARCHAR(1000) NULL,
   PRIMARY KEY (`id`))
@@ -42,13 +42,13 @@ CREATE TABLE IF NOT EXISTS `dbqosqos`.`university` (
   `region` INT NULL,
   `description` VARCHAR(1000) NULL,
   `latitude` FLOAT NULL,
-  `altitude` FLOAT NULL,
+  `longitude` FLOAT NULL,
   `logo` BLOB NULL,
-  `is_verified` TINYINT NULL,
+  `is_verified` TINYINT(1) NULL,
   `doc_verifier` BLOB NULL,
   `reference` VARCHAR(200) NULL,
-  `type_university_id` INT NOT NULL,
-  `album_id` INT NOT NULL,
+  `type_university_id` INT,
+  `album_id` INT,
   PRIMARY KEY (`id`),
   INDEX `fk_university_type_university_idx` (`type_university_id` ASC) VISIBLE,
   INDEX `fk_university_album1_idx` (`album_id` ASC) VISIBLE,
@@ -75,7 +75,7 @@ CREATE TABLE IF NOT EXISTS `dbqosqos`.`faculty` (
   `logo` BLOB NULL,
   `is_verified` TINYINT(1) NULL,
   `doc_verifier` BLOB NULL,
-  `university_id` INT NOT NULL,
+  `university_id` INT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_faculty_university1_idx` (`university_id` ASC) VISIBLE,
   CONSTRAINT `fk_faculty_university1`
@@ -89,7 +89,7 @@ ENGINE = InnoDB;
 -- Table `dbqosqos`.`career`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `dbqosqos`.`career` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `acronym` VARCHAR(50) NULL,
   `name` VARCHAR(200) NULL,
   `description` VARCHAR(1000) NULL,
@@ -97,8 +97,8 @@ CREATE TABLE IF NOT EXISTS `dbqosqos`.`career` (
   `is_verified` TINYINT(1) NULL,
   `doc_verifier` BLOB NULL,
   `reference` VARCHAR(200) NULL,
-  `faculty_id` INT NOT NULL,
-  `album_id` INT NOT NULL,
+  `faculty_id` INT,
+  `album_id` INT,
   PRIMARY KEY (`id`),
   INDEX `fk_career_faculty1_idx` (`faculty_id` ASC) VISIBLE,
   INDEX `fk_career_album1_idx` (`album_id` ASC) VISIBLE,
@@ -124,7 +124,14 @@ CREATE TABLE IF NOT EXISTS `dbqosqos`.`type_course` (
   `description` VARCHAR(1000) NULL,
   `is_verified` TINYINT(1) NULL,
   `doc_verifier` BLOB NULL,
-  PRIMARY KEY (`id`))
+  `university_id` INT,
+  PRIMARY KEY (`id`),
+  INDEX `fk_type_course_university1_idx` (`university_id` ASC) VISIBLE,
+  CONSTRAINT `fk_type_course_university1`
+    FOREIGN KEY (`university_id`)
+    REFERENCES `dbqosqos`.`university` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
@@ -157,20 +164,20 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `dbqosqos`.`course` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `code` VARCHAR(50) NOT NULL,
+  `code` VARCHAR(50),
   `name` VARCHAR(200) NULL,
   `description` VARCHAR(1000) NULL,
   `credits` INT NULL,
-  `group` INT NOT NULL,
-  `year` INT NOT NULL,
+  `group` INT,
+  `year` INT,
   `period` INT NULL,
   `semester` INT NULL,
   `total_hours` INT NULL,
-  `is_verified` TINYINT NULL,
+  `is_verified` TINYINT(1) NULL,
   `doc_verifier` BLOB NULL,
-  `type_course_id` INT NOT NULL,
-  `career_id` INT NOT NULL,
-  `Person_id` INT NOT NULL,
+  `type_course_id` INT,
+  `career_id` INT,
+  `Person_id` INT,
   INDEX `fk_course_type_course1_idx` (`type_course_id` ASC) VISIBLE,
   PRIMARY KEY (`id`),
   INDEX `fk_course_Person1_idx` (`Person_id` ASC) VISIBLE,
@@ -201,7 +208,7 @@ CREATE TABLE IF NOT EXISTS `dbqosqos`.`schedule` (
   `day` INT NULL,
   `is_verified` TINYINT(1) NULL,
   `doc_verifier` BLOB NULL,
-  `course_id` INT NOT NULL,
+  `course_id` INT,
   PRIMARY KEY (`id`),
   INDEX `fk_schedule_course1_idx` (`course_id` ASC) VISIBLE,
   CONSTRAINT `fk_schedule_course1`
@@ -212,31 +219,31 @@ CREATE TABLE IF NOT EXISTS `dbqosqos`.`schedule` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `dbqosqos`.`couses_conection`
+-- Table `dbqosqos`.`courses_connection`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbqosqos`.`couses_conection` (
+CREATE TABLE IF NOT EXISTS `dbqosqos`.`courses_connection` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `year` INT NULL,
   `period` INT NULL,
   `threshold_credits` INT NULL,
-  `career_id` INT NOT NULL,
-  `child` INT NOT NULL,
-  `parent` INT NOT NULL,
+  `career_id` INT,
+  `child` INT,
+  `parent` INT,
   PRIMARY KEY (`id`),
-  INDEX `fk_couse_conection_career1_idx` (`career_id` ASC) VISIBLE,
-  INDEX `fk_couses_conection_course1_idx` (`child` ASC) VISIBLE,
-  INDEX `fk_couses_conection_course2_idx` (`parent` ASC) VISIBLE,
-  CONSTRAINT `fk_couse_conection_career1`
+  INDEX `fk_courses_connection_career1_idx` (`career_id` ASC) VISIBLE,
+  INDEX `fk_courses_connection_course1_idx` (`child` ASC) VISIBLE,
+  INDEX `fk_courses_connection_course2_idx` (`parent` ASC) VISIBLE,
+  CONSTRAINT `fk_courses_connection_career1`
     FOREIGN KEY (`career_id`)
     REFERENCES `dbqosqos`.`career` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_couses_conection_course1`
+  CONSTRAINT `fk_courses_connection_course1`
     FOREIGN KEY (`child`)
     REFERENCES `dbqosqos`.`course` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_couses_conection_course2`
+  CONSTRAINT `fk_courses_connection_course2`
     FOREIGN KEY (`parent`)
     REFERENCES `dbqosqos`.`course` (`id`)
     ON DELETE NO ACTION
@@ -249,7 +256,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `dbqosqos`.`career_person` (
   `career_id` INT NOT NULL,
   `person_id` INT NOT NULL,
-  `is_verifier` TINYINT(1) NULL,
+  `is_verified` TINYINT(1) NULL,
   `doc_verifier` BLOB NULL,
   PRIMARY KEY (`career_id`, `person_id`),
   INDEX `fk_career_has_person_person1_idx` (`person_id` ASC) VISIBLE,
@@ -273,10 +280,10 @@ CREATE TABLE IF NOT EXISTS `dbqosqos`.`enrollment` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `status` INT NULL,
   `grade` FLOAT NULL,
-  `is_verified` TINYINT NULL,
+  `is_verified` TINYINT(1) NULL,
   `doc_verifier` BLOB NULL,
-  `person_id` INT NOT NULL,
-  `course_id` INT NOT NULL,
+  `person_id` INT,
+  `course_id` INT,
   PRIMARY KEY (`id`),
   INDEX `fk_enrollment_person1_idx` (`person_id` ASC) VISIBLE,
   INDEX `fk_enrollment_course1_idx` (`course_id` ASC) VISIBLE,
@@ -299,7 +306,8 @@ CREATE TABLE IF NOT EXISTS `dbqosqos`.`contribution` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `date` DATE NULL,
   `score` INT NULL,
-  `person_id` INT NOT NULL,
+  `description` VARCHAR(1000) NULL,
+  `person_id` INT,
   PRIMARY KEY (`id`),
   INDEX `fk_contribution_person1_idx` (`person_id` ASC) VISIBLE,
   CONSTRAINT `fk_contribution_person1`
@@ -315,8 +323,8 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `dbqosqos`.`interest` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `tag` VARCHAR(100) NULL,
-  `skill` TINYINT(1) NULL,
-  `person_id` INT NOT NULL,
+  `is_skill` TINYINT(1) NULL,
+  `person_id` INT,
   PRIMARY KEY (`id`),
   INDEX `fk_Interest_person1_idx` (`person_id` ASC) VISIBLE,
   CONSTRAINT `fk_Interest_person1`
@@ -330,7 +338,7 @@ ENGINE = InnoDB;
 -- Table `dbqosqos`.`story`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `dbqosqos`.`story` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(100) NULL,
   `body` VARCHAR(500) NULL,
   `recommended` INT NULL,
@@ -340,7 +348,8 @@ CREATE TABLE IF NOT EXISTS `dbqosqos`.`story` (
   `doc_verifier` BLOB NULL,
   `allow_comments` TINYINT(1) NULL,
   `layer` INT NULL,
-  `person_id` INT NOT NULL,
+  `date` DATE NULL,
+  `person_id` INT,
   PRIMARY KEY (`id`),
   INDEX `fk_story_person1_idx` (`person_id` ASC) VISIBLE,
   CONSTRAINT `fk_story_person1`
@@ -355,11 +364,12 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `dbqosqos`.`comment` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `content` VARCHAR(1000) NOT NULL,
+  `content` VARCHAR(1000),
   `like` INT NULL,
   `dislike` INT NULL,
-  `story_id` INT NOT NULL,
-  `person_id` INT NOT NULL,
+  `date` DATE NULL,
+  `story_id` INT,
+  `person_id` INT,
   PRIMARY KEY (`id`),
   INDEX `fk_comment_story1_idx` (`story_id` ASC) VISIBLE,
   INDEX `fk_comment_person1_idx` (`person_id` ASC) VISIBLE,
@@ -376,9 +386,9 @@ CREATE TABLE IF NOT EXISTS `dbqosqos`.`comment` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `dbqosqos`.`Award`
+-- Table `dbqosqos`.`award`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbqosqos`.`Award` (
+CREATE TABLE IF NOT EXISTS `dbqosqos`.`award` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `year` INT NULL,
   `period` INT NULL,
@@ -387,17 +397,17 @@ CREATE TABLE IF NOT EXISTS `dbqosqos`.`Award` (
   `place` INT NULL,
   `is_verified` TINYINT(1) NULL,
   `doc_verifier` BLOB NULL,
-  `career_id` INT NOT NULL,
-  `album_id` INT NOT NULL,
+  `career_id` INT,
+  `album_id` INT,
   PRIMARY KEY (`id`),
-  INDEX `fk_Award_career1_idx` (`career_id` ASC) VISIBLE,
-  INDEX `fk_Award_album1_idx` (`album_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Award_career1`
+  INDEX `fk_award_career1_idx` (`career_id` ASC) VISIBLE,
+  INDEX `fk_award_album1_idx` (`album_id` ASC) VISIBLE,
+  CONSTRAINT `fk_award_career1`
     FOREIGN KEY (`career_id`)
     REFERENCES `dbqosqos`.`career` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Award_album1`
+  CONSTRAINT `fk_award_album1`
     FOREIGN KEY (`album_id`)
     REFERENCES `dbqosqos`.`album` (`id`)
     ON DELETE NO ACTION
@@ -411,7 +421,7 @@ CREATE TABLE IF NOT EXISTS `dbqosqos`.`photo` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `pic` BLOB NULL,
   `description` VARCHAR(200) NULL,
-  `album_id` INT NOT NULL,
+  `album_id` INT,
   PRIMARY KEY (`id`),
   INDEX `fk_photo_album1_idx` (`album_id` ASC) VISIBLE,
   CONSTRAINT `fk_photo_album1`
@@ -432,12 +442,19 @@ CREATE TABLE IF NOT EXISTS `dbqosqos`.`tag_container` (
   `reference` VARCHAR(200) NULL,
   `type` INT NULL,
   `layer` INT NULL,
-  `career_id` INT NOT NULL,
+  `career_id` INT,
+  `person_id` INT,
   PRIMARY KEY (`id`),
-  INDEX `fk_requirement_career1_idx` (`career_id` ASC) VISIBLE,
-  CONSTRAINT `fk_requirement_career1`
+  INDEX `fk_tag_container_career1_idx` (`career_id` ASC) VISIBLE,
+  INDEX `fk_ta_container_person1_idx` (`person_id` ASC) VISIBLE,
+  CONSTRAINT `fk_tag_container_career1`
     FOREIGN KEY (`career_id`)
     REFERENCES `dbqosqos`.`career` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  CONSTRAINT `fk_tag_container_person1`
+    FOREIGN KEY (`person_id`)
+    REFERENCES `dbqosqos`.`person` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
