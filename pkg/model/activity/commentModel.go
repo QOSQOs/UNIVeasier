1a20178c04b40a33242c68f37b4f931a7919845a
darwinTtito <darwin.ttito.c@gmail.com>
2019-10-14 22:39:11 -0300

    fix: enhance the model based on the recent changes done in the database

--- pkg/model/activity/commentModel.go
+++ pkg/model/activity/commentModel.go
@@ -1,10 +1,13 @@
 package activity
 
+import "time"
+
 type Comment struct {
-	Id       uint64 `json:"id"`
-	Content  string `json:"content"`
-	Like     string `json:"like"`
-	Dislike  int64  `json:"dislike"`
-	StoryId  int64  `json:"story_id"`
-	PersonId uint64 `json:"person_id"`
+	Id       uint64    `json:"id"`
+	Content  string    `json:"content"`
+	Like     string    `json:"like"`
+	Dislike  int64     `json:"dislike"`
+	Date     time.Time `json:"date"`
+	StoryId  uint64    `json:"story_id"`
+	PersonId uint64    `json:"person_id"`
 }
--- pkg/model/activity/contributionModel.go
+++ pkg/model/activity/contributionModel.go
@@ -3,8 +3,9 @@ package activity
 import "time"
 
 type Contribution struct {
-	Id       uint64    `json:"id"`
-	Date     time.Time `json:"date"`
-	Score    int64     `json:"score"`
-	PersonId uint64    `json:"person_id"`
+	Id          uint64    `json:"id"`
+	Date        time.Time `json:"date"`
+	Score       int64     `json:"score"`
+	Description string    `json:"description"`
+	PersonId    uint64    `json:"person_id"`
 }
