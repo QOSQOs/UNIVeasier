f6faf10ed30fdcc058cb44274bbb900231caf131
Edison Kleiber Ttito <Kleiber100892@gmail.com>
2019-10-17 22:18:01 -0300

    fix: improve way to reference configuration file (#28)

--- cmd/univeasier.go
+++ cmd/univeasier.go
@@ -1,17 +1,13 @@
 package main
 
 import (
+	"os"
+
 	"github.com/QOSQOs/UNIVeasier/pkg/api"
 )
 
 func main() {
-	/*
-		filename:= flag.String("config", "", "References to configuration file.")
-		flag.Parse()
-		s.Initialize(*filename)
-	*/
-
-	configPath := `C:\Users\i865255\go\src\github.com\QOSQOs\UNIVeasier\internal\config\config.json`
+	configPath := os.Getenv("CONFIG")
 
 	s := api.Server{}
 	s.Initialize(configPath)
